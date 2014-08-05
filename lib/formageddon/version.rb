6ad63c382afaf1a2fc49944d03f1807ba64bb986
Clayton Dunwell <clayton.dunwell@gmail.com>
2014-08-05 14:51:54 -0400

    fixing silliness

--- lib/formageddon/version.rb
+++ lib/formageddon/version.rb
@@ -1,3 +1,3 @@
 module Formageddon
-  VERSION = IO.read(File.join(File.dirname(__FILE__), "../../","README")) || "0.0.1"
+  VERSION = IO.read(File.join(File.dirname(__FILE__), "../../", "VERSION")) || "0.0.1"
 end
