e9948702c0a13df872cab956eecbfef88b6f4523
Sam Handler <sam.handler@gmail.com>
2014-08-08 10:26:49 -0400

    Bump version in another place

--- lib/formageddon/version.rb
+++ lib/formageddon/version.rb
@@ -1,3 +1,3 @@
-module Formageddon
-  VERSION = IO.read(File.join(File.dirname(__FILE__), "../../", "VERSION")) || "0.0.1"
+lsmodule Formageddon
+  VERSION = IO.read(File.join(File.dirname(__FILE__), "../../", "VERSION")) || "0.0.2"
 end
